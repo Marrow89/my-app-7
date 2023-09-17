405a1fecd7c5bde03df1fe7680aa371c896f5c5b
Marrow89 <139663175+Marrow89@users.noreply.github.com>
2023-09-17 12:46:18 -0700

    Updating and Testing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function currentDate(now) {
-  let currentDay = now.getDay();
+  let currentDay = now.getDays();
   let currentHour = now.getHours();
  if(currentHour < 10) {
     currentHour = `0${currentHour}`;
@@ -68,7 +68,7 @@ windElement.innerHTML =Math.round(response.data.wind.speed);
 cityInput.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
-dateElement.innerHTML = currentDate(response.data.dt * 1000);
+dateElement.innerHTML = currentDayElement(response.data.dt * 1000);
 iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
 h1.innerHTML = `It is currently ${temperature}° in ${response.data.name}`;
