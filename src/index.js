7dae6f6b482893219455e326ef4f60392e741204
Marrow89 <139663175+Marrow89@users.noreply.github.com>
2023-09-17 15:19:42 -0700

    Chachachachanges

--- src/index.js
+++ src/index.js
@@ -78,6 +78,8 @@ strongElement.innerHTML = `${temperature}`;
 
 function displayFahrenheitTemperature(event) {
 event.preventDefault();
+celsiusLink.classList.remove("active");
+fahrenheitLink.classList.add("active");
 let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
 let temperatureElement = document.querySelector("#temperature");
 temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
@@ -85,6 +87,8 @@ temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 
 function displayCelsiusTemperature(event){
   event.preventDefault();
+  celsiusLink.classList.add("active");
+fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celciusTemperature);
 }
@@ -100,3 +104,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
+searchCity("Redding");
