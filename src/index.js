809ba133cb090aa1bcdec711beccbd14a9c77403
Marrow89 <139663175+Marrow89@users.noreply.github.com>
2023-09-26 19:12:39 -0700

    Updated, tested some code
    Code broke, this is on the mend

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-function currentDate(now) {
-  let currentDay = now.getDays();
-  let currentHour = now.getHours();
- if(currentHour < 10) {
-    currentHour = `0${currentHour}`;
+function currentDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+ if(hours < 10) {
+    hours = `0${currentHour}`;
   }
-  let currentMinute = now.getMinutes();
-  if(currentMinute < 10) {
-    currentMinute = `0${currentMinute}`;
+  let minutes = date.getMinutes();
+  if(minutes < 10) {
+    minutes = `0${minutes}`;
   }
 
 let days = [
@@ -18,79 +18,53 @@ let days = [
     "Friday",
     "Saturday"
   ];
-  let currentDayElement = document.getElementById("currentDay");
-  currentDayElement.textContent = days[currentDay];
 
-  let currentHourElement = document.getElementById("currentHour");
-  currentHourElement.textContent = currentHour + ":";
+   let currentDateElement = document.getElementById("currentDay");
+currentDayElement.textContent = days[currentDay];
 
-  let currentMinuteElement = document.getElementById("currentMinute");
-  currentMinuteElement.textContent = currentMinute;
-}
 
-function displayForecast() {
-  let forecastElement = document.querySelector("#forecast");
+let currentHourElement = document.getElementById("currentHour");
+currentHourElement.textContent = currentHour + ":";
 
-  let forecastHTML = `<div class="row">`;
-let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
-days.forEach(function(day){
 
-  forecastHTML = 
-   forecastHTML + `
-          <div class="col-2">
-                <div class="weather-forecast-date"></div>
-                ${day}
-                <img
-                  src="https://cityweatherteller.netlify.app/img/cool.jpeg"
-                  alt=""
-                  width="38px"
-                />
-                <div class="weather-forecast-temperature">
-                  <span class="weather-forecast-temperature-max">99°</span>
-                  <span class="weather-forecast-temperature-min">76°</span>
-                </div>
-              
-          
-          </div>`;
-})
-
-   
-
-          forecastHTML = forecastHTML + `</div>`;
-
-          forecastElement.innerHTML = forecastHTML;
-}
+let currentMinuteElement = document.getElementById("currentMinute");
+currentMinuteElement.textContent = currentMinute;
+};
 
 function showTemperature(response) {
 
   let h1 = document.querySelector("h1");
-  let temperature = Math.round(response.data.main.temp);
-  let cityInput = response.data.name;
+  let temperatureElement = Math.round("#temperature");
+  let cityElement = document.querySelector("city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let iconElement = document.querySelector("#icon");
-  let strongElement = document.querySelector("#temperature");
-
+  
   let celsiusTemperature = Math.round(response.data.main.temp);
 
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
   windElement.innerHTML =Math.round(response.data.wind.speed);
-  cityInput.innerHTML = response.data.name;
+  cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
-  temperature.innerHTML = Math.round(celsiusTemperature);
+  
+
 
 iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt", response.data.weather[0].description);
 h1.innerHTML = `${response.data.name}`;
-strongElement.innerHTML = `${temperature}`;
+strongElement.innerHTML = `${temperatureElement}`;
 }
+
+
 
 
 function searchCity(city) {
 let cityInput = document.querySelector("#city-input").value;
 let apiKey = "e6c2364656962bdcb16bc352fc42569a";
-let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`;
+let apiUrl =`
+https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`;
 
  axios.get(apiUrl).then(showTemperature);
 }
@@ -103,14 +77,13 @@ function handleSubmit(event) {
 
 }
 
-
 function displayFahrenheitTemperature(event) {
 event.preventDefault();
 let temperatureElement = document.querySelector("#temperature");
 
 celsiusLink.classList.remove("active");
 fahrenheitLink.classList.add("active");
-let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+let fahrenheitTemperature = (celsiusTemperature);
 temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
@@ -133,5 +106,3 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-searchCity("Redding");
-displayForecast();
